944096dbda3e3789e67ba1d203e37faefae79fec
Lauren <lauren.m.lavelle@wmich.edu>
2023-09-07 19:15:41 -0400

    login route starter code written

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -2,39 +2,61 @@ const router = require("express").Router();
 // will need path for user model
 const { User } = require();
 
-// Create route to render view of signup page 
+// Create route to render view of signup page
 
 // Create new user
 // make sure variables correspond with user model once created
 router.get("/signup", async (req, res) => {
-    try {
+  try {
     const dbUserData = await User.create({
-        first_name: req.body.firstName,
-        last_name: req.body.lastName,
-        username: req.body.username,
-        password: req.body.password,
+      first_name: req.body.firstName,
+      last_name: req.body.lastName,
+      username: req.body.username,
+      password: req.body.password,
     });
     req.session.save(() => {
-        req.session.loggedIn = true;
-        req.session.userId = db.UserData.id;
-        res.status(200).json(dbUserData);
+      req.session.loggedIn = true;
+      req.session.userId = db.UserData.id;
+      res.status(200).json(dbUserData);
     });
-    } catch (err) {
+  } catch (err) {
     console.log(err);
     res.status(500).json(err);
-    }
+  }
 });
 
 // create view to render login page
 
-
 // Login
-router.post("/login", async(req,res)=>{
-    try{
-        const dbUserData = await User.findOne({
-            where:{
-                username: req.body.username
-            }
-        })
+// check with model for variables
+router.post("/login", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        username: req.body.username,
+      },
+    });
+    if (!dbUserData) {
+      res.status(400).json({ message: "Incorrect username or password." });
+      return;
     }
-})
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect username or password." });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.userId = dbUserData.id;
+      res
+        .status(200)
+        .json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});

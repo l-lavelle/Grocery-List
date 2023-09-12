f6a215d29615d6d713fecc1708f78c08a5043e78
Lauren <lauren.m.lavelle@wmich.edu>
2023-09-11 20:53:04 -0400

    changing branches

--- controllers/api/listRoutes.js
+++ controllers/api/listRoutes.js
@@ -2,29 +2,28 @@ const router = require("express").Router();
 // will need path for list model
 const { User, List, Product, ListProduct } = require("../../models");
 
-router.get('/', async (req, res) => {
-    try {
-        const listData = await List.findAll({
-            include: [{ model: User }],
-        });
-        res.status(200).json(listData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const listData = await List.findAll({
+      include: [{ model: User }],
+    });
+    res.status(200).json(listData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/:user_id', async (req, res) => {
-    try {
-        const listData = await List.create({
-            name: req.body.name,
-            user_id: req.params.user_id,
-
-        });
-        res.status(200).json(listData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+router.post("/:user_id", async (req, res) => {
+  try {
+    const listData = await List.create({
+      name: req.body.name,
+      user_id: req.params.user_id,
+    });
+    res.status(200).json(listData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;

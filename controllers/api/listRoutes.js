566904783047307de8b700648093a6d909d3cf25
Lauren <lauren.m.lavelle@wmich.edu>
2023-09-13 19:12:13 -0400

    list page renders with current list

--- controllers/api/listRoutes.js
+++ controllers/api/listRoutes.js
@@ -2,12 +2,22 @@ const router = require("express").Router();
 // will need path for list model
 const { User, List, Product, ListProduct } = require("../../models");
 
-router.get("/", async (req, res) => {
+const withAuth = require("../../utils/auth");
+
+router.get("/:user_id", withAuth, async (req, res) => {
   try {
     const listData = await List.findAll({
       include: [{ model: User }],
+      where: {
+        user_id: req.params.user_id,
+      },
     });
-    res.status(200).json(listData);
+    const lists = listData.map((posts) => posts.get({ plain: true }));
+    res.render("list", {
+      lists,
+      loggedIn: Boolean(req?.session?.loggedIn),
+      userId: req?.session?.userId,
+    });
   } catch (err) {
     res.status(500).json(err);
   }

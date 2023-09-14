c17c67a170e281d9abfa140f04ec653b27f9b6ec
Lauren <lauren.m.lavelle@wmich.edu>
2023-09-14 13:10:18 -0400

    create get product by food_id route

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -2,7 +2,22 @@ const router = require("express").Router();
 // will need path for list model
 const { User, List, Product, ListProduct } = require("../../models");
 
-router.get("/", async (req, res) => {});
+// get product by food_id
+router.get("/", async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      where: {
+        food_id: req.body.foodId,
+      },
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// creates new product
 router.post("/", async (req, res) => {
   try {
     const productData = await Product.create({
